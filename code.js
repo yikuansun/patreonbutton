5d91b60bd8ce85315b2e179b1f0c570afe18e727
Yikuan Sun <38194363+yikuansun@users.noreply.github.com>
2022-06-14 10:55:43 -0400

    Make copy to clipboard button

--- code.js
+++ code.js
@@ -5,3 +5,8 @@ document.querySelector("#username").addEventListener("input", function() {
     document.querySelector("#button_code a").href = `https://www.patreon.com/join/${this.value}?`;
     document.querySelector("#toCopy").value = document.querySelector("#button_code").innerHTML;
 });
+
+document.querySelector("#copybutton").addEventListener("click", function() {
+    navigator.clipboard.writeText(document.querySelector("#toCopy").value);
+    document.querySelector("#toCopy").select();
+});
