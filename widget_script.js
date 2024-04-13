188aa07d153b0ab2941b8125ff7254b81d9182e9
ulf-frostypaw <19324157@uagro.mx>
2024-04-12 22:23:37 -0600

    Actualizacion de estilos y se añadio PicoCSS.

--- widget_script.js
+++ widget_script.js
@@ -2,4 +2,4 @@ var hLink = document.querySelector("a");
 var uname = (new URLSearchParams(location.search)).get("uname");
 if (!uname) document.body.innerHTML = "Error: no uname param";
 
-hLink.href = `https://www.patreon.com/join/${uname}?`;
+hLink.href = `https://www.patreon.com/${uname}`;
