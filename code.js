e24a3032e574f8bd4d7b2327d8352d932834f90f
Yikuan Sun <yikuansun2015@gmail.com>
2024-04-13 14:27:46 -0400

    Update urls

--- code.js
+++ code.js
@@ -2,7 +2,7 @@ document.querySelector("#button_code").innerHTML = "" + document.querySelector("
 document.querySelector("#toCopy").value = document.querySelector("#button_code").innerHTML;
 
 document.querySelector("#username").addEventListener("input", function() {
-    document.querySelector("#button_code iframe").src = `https://ulf-frostypaw.github.io/patreon-button-widget/widget.html?username=${this.value}`;
+    document.querySelector("#button_code iframe").src = `https://yikuansun.github.io/patreonbutton/widget.html?username=${this.value}`;
     document.querySelector("#toCopy").value = document.querySelector("#button_code").innerHTML;
 });
 
